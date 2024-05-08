201edcaa818d60bcabdd64d9cc1e0b9f212203cf
bipey <113363191+bipey@users.noreply.github.com>
2024-05-08 08:12:54 +0545

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const path = require('path');
 const moment = require('moment-timezone');
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'public')));
 // Getting the required credentials
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
